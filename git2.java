e11b0f94a80034341b4363a00c93d4b8eea2829c
SATYA PRKASH <96809659+satya8574@users.noreply.github.com>
2022-01-03 22:57:24 +0530

    Add files via upload

---
+++ git2.java
@@ -0,0 +1,9 @@
+<body>  
+    <form action="">  
+        <label>Enter the Username</label>  
+        <input type="text" name="username" />  
+        <label>Enter the Password</label>  
+        <input type="password" name="password"  />  
+        <input type="submit" value="Login" name="submit" class="submit" />  
+    </form>  
+</body>  
